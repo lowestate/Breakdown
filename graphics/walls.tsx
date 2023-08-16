024abcc2658b43f334ce94dcd1f5d9d5958e77a2
yan <bents.yan1@gmail.com>
2023-08-16 22:44:50 +0300

    fresh files

---
+++ graphics/walls.tsx
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="walls" tilewidth="32" tileheight="32" tilecount="63" columns="9">
+ <image source="red_1.png" width="288" height="224"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="corner" tile="-1">
+   <wangcolor name="red" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="black" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="1" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="2" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="9" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="10" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="11" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="13" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="18" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="19" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="20" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="27" wangid="0,2,0,1,0,2,0,1"/>
+   <wangtile tileid="28" wangid="0,1,0,2,0,1,0,2"/>
+   <wangtile tileid="29" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="36" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="37" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="38" wangid="0,2,0,2,0,1,0,2"/>
+  </wangset>
+ </wangsets>
+</tileset>
